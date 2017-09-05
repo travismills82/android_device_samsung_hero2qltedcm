ecbfc3047ae384b63fb8bebbfd3e4edbad13d681
LuK1337 <priv.luk@gmail.com>
2017-09-05 13:46:00 -0400

    sensors: Adjust for our usecase
    * Rename multihal -> multihal-samsung8996 so soong
      doesn't complain about duplicate package names.
    * Remove USE_SENSOR_MULTI_HAL flag requirement so
      we can actually build it.
    * Use TARGET_BOARD_PLATFORM variable in package
      name.
    
    Change-Id: Ifa8c20747e3f0bf9bae8b42dd4c499cf6d770e27

--- device.mk
+++ device.mk
@@ -308,6 +308,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8996
+
 # SPN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
