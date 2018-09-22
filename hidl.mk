9dffb7cb862fc812ed332ca8965737c73ef5694d
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2018-09-23 02:44:04 +0900

    build separate system and vendor hidl libs

--- hidl.mk
+++ hidl.mk
@@ -71,7 +71,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # IR Blaster
 PRODUCT_PACKAGES += \
