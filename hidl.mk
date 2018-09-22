6c17c1f7f630943c2dfff51baa59045edfc1a327
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2018-09-22 23:09:26 +0900

    configstore: Uprev configstore (1.0 -> 1.1)

--- hidl.mk
+++ hidl.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 
 # Display/Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service \
+    android.hardware.configstore@1.1-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
