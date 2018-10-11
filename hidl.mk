202a8ab1758591b6c575debbe58d1027bea8a05f
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2018-10-12 00:09:50 +0900

    Drop vendor.display.color and Add vendor.display.config

--- hidl.mk
+++ hidl.mk
@@ -41,8 +41,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.color@1.0-impl \
-    vendor.display.color@1.0-service
+    vendor.display.config@1.1 \
+    vendor.display.config@1.1_vendor
 
 # DRM
 PRODUCT_PACKAGES += \
