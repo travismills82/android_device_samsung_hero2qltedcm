e1ff5f48aea0174954bd87716f0ffc788d446a81
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2018-10-12 00:36:32 +0900

    Add Media Extensions

--- device.mk
+++ device.mk
@@ -252,6 +252,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Media Extensions
+PRODUCT_PACKAGES += \
+    libmediametrics \
+    libregistermsext
+
 # Netutils
 PRODUCT_PACKAGES += \
     libandroid_net \
