b802287b350c6dcc4830ce9079941ed2c0e6cf26
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2018-10-24 02:44:17 +0900

    Update gps and location from LA.UM.7.5.r1-02500-8x96.0

--- gps/Android.mk
+++ gps/Android.mk
@@ -15,4 +15,9 @@
 # limitations under the License.
 #
 
-include $(call first-makefiles-under,$(call my-dir))
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/build/target_specific_features.mk
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
