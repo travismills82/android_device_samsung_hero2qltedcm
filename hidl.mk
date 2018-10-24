845382637a075d82c4b28e5262c382284b344708
miraclestars <miracle.star.t.n.5.dec@gmail.com>
2018-10-25 02:00:25 +0900

    Build binderized samsung8996 sensors service

--- hidl.mk
+++ hidl.mk
@@ -125,8 +125,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl.samsung8996 \
+    android.hardware.sensors@1.0-service.samsung8996
 
 # Thermal
 PRODUCT_PACKAGES += \
